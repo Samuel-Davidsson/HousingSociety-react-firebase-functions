0c9139f2f4a5d5160f33d99372641e0db0eeeaed
Samuel <sam.davidsson86@outlook.com>
2019-06-25 15:47:48 +0200

    Signup added

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,21 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const express = require("express");
+const app = require("express")();
 
 admin.initializeApp();
 
-const app = express();
+const firebaseConfig = {
+    //Insert your config here
+  };
+
+
+const firebase = require("firebase");
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
 
 app.get("/posts", (req, res) => {
-    admin.firestore()
+     db
     .collection("posts")
     .orderBy("createdAt","desc")
     .get()
@@ -34,7 +42,7 @@ app.post("/post", (req, res) => {
         createdAt: new Date().toISOString()
     };
 
-    admin.firestore()
+    db    
     .collection("posts")
     .add(newPost)
     .then(doc => {
@@ -43,6 +51,53 @@ app.post("/post", (req, res) => {
     .catch(err => {
         res.status(500).json({error: "Something went wrong"});
     })
+});
+
+app.post("/signup", (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmpassword: req.body.confirmpassword,
+        handle: req.body.handle
+    };
+
+    let token, userId;
+    db.doc(`/users/${newUser.handle}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return res.status(400).json({ handle: 'this handle is already taken' });
+        } else {
+          return firebase
+            .auth()
+            .createUserWithEmailAndPassword(newUser.email, newUser.password);
+        }
+      })
+      .then((data) => {
+        userId = data.user.uid;
+        return data.user.getIdToken();
+      })
+      .then((idToken) => {
+        token = idToken;
+        const userCredentials = {
+          handle: newUser.handle,
+          email: newUser.email,
+          createdAt: new Date().toISOString(),
+          userId
+        };
+        return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+      })
+      .then(() => {
+        return res.status(201).json({ token });
+      })
+      .catch((err) => {
+        console.error(err);
+        if (err.code === 'auth/email-already-in-use') {
+          return res.status(400).json({ email: 'Email is already is use' });
+        } else {
+          return res.status(500).json({ error: err.code });
+        }
+      });
 });
 
 exports.api = functions.region("europe-west1").https.onRequest(app);
