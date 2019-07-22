aacb3eb627277205b74ebe6c1d6412965e9ff0a0
Samuel <sam.davidsson86@outlook.com>
2019-07-22 23:47:59 +0200

    GetPost and CommentPost added

--- functions/handlers/posts.js
+++ functions/handlers/posts.js
@@ -45,4 +45,59 @@ exports.addPost =  (req, res) => {
           res.status(500).json({error: "Something went wrong"});
       });
   };
+
+exports.getPost = (req, res) => {
+    let postData = {};
+
+    db.doc(`/posts/${req.params.postId}`).get()
+    .then(doc => {
+        if(!doc.exists) {
+            return res.status(404).json({error: "Post not found!"})
+        }
+        postData = doc.data();
+        postData.postId = doc.id;
+        
+        return db.collection("comments").where("postId", "==", req.params.postId).get();
+    })
+    .then(data => {
+        postData.comments = [];
+        data.forEach(doc =>{
+            postData.comments.push(doc.data())
+        });
+            return res.json(postData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({error: "Something went wrong"});
+    });
+};
+
+exports.commentOnPost = (req, res) => {
+    if (req.body.body.trim() === "")
+      return res.status(400).json({ error: "Must not be empty" });
+  
+    const newComment = {
+      body: req.body.body,
+      createdAt: new Date().toISOString(),
+      postId: req.params.postId,
+      userHandle: req.user.handle,
+      userImage: req.user.imageUrl
+    };
+
+    db.doc(`/posts/${req.params.postId}`)
+      .get()
+      .then(doc => {
+        if (!doc.exists) {
+          return res.status(404).json({ error: "Post not found" });
+        }
+        return db.collection("comments").add(newComment);       
+      })
+      .then(() => {
+        res.json(newComment);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ error: "Something went wrong" });
+      });
+  };
   
--- functions/index.js
+++ functions/index.js
@@ -2,11 +2,17 @@ const functions = require("firebase-functions");
 const app = require("express")();
 
 const FBAuth = require("./util/fbAuth");
-const { getAllPosts, addPost } = require("./handlers/posts")
+const { getAllPosts, addPost, getPost, commentOnPost } = require("./handlers/posts")
 const {signup, login, uploadImage, addUserDetails, getAuthenticatedUser} = require("./handlers/users");
 
 app.get("/posts", getAllPosts);
+app.get("/post/:postId", getPost);
 app.post("/post", FBAuth, addPost);
+app.post("/post/:postId/comment", FBAuth, commentOnPost);
+//TODO delete
+//TODO like a post
+//TODO unlike a post
+
 
 app.post("/login", login);
 app.post("/signup", signup);
