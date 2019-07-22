818a5ed421803a36c6f1ae722b49caca8d881b9b
Samuel <sam.davidsson86@outlook.com>
2019-07-22 15:31:35 +0200

    Added Image route

--- functions/index.js
+++ functions/index.js
@@ -3,13 +3,13 @@ const app = require("express")();
 
 const FBAuth = require("./util/fbAuth");
 const { getAllPosts, addPost } = require("./handlers/posts")
-const {signup, login} = require("./handlers/users");
+const {signup, login, uploadImage} = require("./handlers/users");
 
 app.get("/posts", getAllPosts);
 app.post("/post", FBAuth, addPost);
 
 app.post("/login", login);
 app.post("/signup", signup);
-// app.post("/user/image", FBAuth, uploadImage);
+app.post("/user/image", FBAuth, uploadImage);
 
 exports.api = functions.region("europe-west1").https.onRequest(app);
