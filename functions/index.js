ad3bfc82519a568a885db9728a3e6c55865c1aa7
Samuel <sam.davidsson86@outlook.com>
2019-07-23 23:07:55 +0200

    Like unlike and delete Post

--- functions/index.js
+++ functions/index.js
@@ -2,17 +2,16 @@ const functions = require("firebase-functions");
 const app = require("express")();
 
 const FBAuth = require("./util/fbAuth");
-const { getAllPosts, addPost, getPost, commentOnPost } = require("./handlers/posts")
-const {signup, login, uploadImage, addUserDetails, getAuthenticatedUser} = require("./handlers/users");
+const { getAllPosts, addPost, getPost, commentOnPost, likePost, unlikePost, deletePost } = require("./handlers/posts")
+const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require("./handlers/users");
 
 app.get("/posts", getAllPosts);
 app.get("/post/:postId", getPost);
+app.get("/post/:postId/like", FBAuth, likePost);
+app.get("/post/:postId/unlike", FBAuth, unlikePost);
 app.post("/post", FBAuth, addPost);
 app.post("/post/:postId/comment", FBAuth, commentOnPost);
-//TODO delete
-//TODO like a post
-//TODO unlike a post
-
+app.delete("/post/:postId", FBAuth, deletePost);
 
 app.post("/login", login);
 app.post("/signup", signup);
